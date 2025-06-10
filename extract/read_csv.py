2dc0aac4046d2fa62e4e442ff6e98fc78eb6dadd
Juan D. Bravo <196306536+juan-dbravo@users.noreply.github.com>
2025-06-10 07:13:06 +0000

    fix: correct script name from clean_csv.py to read_csv.py

---
+++ extract/read_csv.py
@@ -0,0 +1,23 @@
+
+from pathlib import Path
+import pandas as pd
+
+
+def read_csv (file: Path) -> pd.DataFrame:
+    """
+    Reads the raw spanish original report and convert to utf-8
+    """
+
+    try:
+        df = pd.read_csv(file, encoding="latin-1", sep=";")
+        print(df.head())
+        return df
+    except Exception as e:
+        raise RuntimeError(f"Failed to read the file {file}: {e}")
+    
+
+
+if __name__ == "__main__":
+
+    file_path = Path('InformeConsumos_Mayo.csv')
+    report = read_csv(file_path)
