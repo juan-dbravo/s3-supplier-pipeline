ca787d39198e47dca0ba635e00dab7a03f1c607d
juan-dbravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-10 06:59:22 +0200

    feat: add function read_csv to anonymizer.py

--- anonymizer.py
+++ anonymizer.py
@@ -5,14 +5,25 @@ import re
 
 
 
-def read_report (file: Path) -> pd.DataFrame:
+def read_and_decode (file: Path) -> pd.DataFrame:
     """
-    Reads the raw spanish original report and convert to utg"""
+    Reads the raw spanish original report and convert to utf-8
+    """
+
     try:
         df = pd.read_csv(file, encoding="latin-1", sep=";")
-        # Drop rows where the first column (supplier) is NaN
-        df = df.dropna(subset=[df.columns[0]])
+        print(df.head())
         return df
     except Exception as e:
         raise RuntimeError(f"Failed to read the file {file}: {e}")
+    
 
+if __name__ == "__main__":
+
+    file_path = Path('InformeConsumos_Mayo.csv')
+    report = read_and_decode(file_path)
+
+
+
+# Drop rows where the first column (supplier) is NaN
+# # df = df.dropna(subset=[df.columns[0]])
